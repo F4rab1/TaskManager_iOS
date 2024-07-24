d4b983265f7b1a87d446dad7262d322038b38b73
F4rab1 <issafarabi2022@gmail.com>
2024-07-24 19:29:47 +0500

    Initial Commit

---
+++ TaskManager_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskManager_iOS
+//
+//  Created by Фараби Иса on 24.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
