0e7332c7b9268acd4bfb4db30b2506c85950565b
F4rab1 <issafarabi2022@gmail.com>
2024-07-26 23:59:12 +0500

    Calendar Day Cell

--- TaskManager_iOS/Controllers/Calendar/CalendarHorizontalController.swift
+++ TaskManager_iOS/Controllers/Calendar/CalendarHorizontalController.swift
@@ -10,15 +10,11 @@ import UIKit
 class CalendarHorizontalController: HorizontalSnappingController, UICollectionViewDelegateFlowLayout {
     
     let dayCellId = "DayCell"
+    private var selectedIndexPath: IndexPath? = IndexPath(item: 0, section: 0)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.backgroundColor = .yellow
-        
-        let indexPath = IndexPath(item: 2, section: 0)
-        DispatchQueue.main.async {
-            self.collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
-        }
         
         collectionView.register(CalendarDayCell.self, forCellWithReuseIdentifier: dayCellId)
         collectionView.contentInset = .init(top: 0, left: 16, bottom: 0, right: 16)
@@ -35,11 +31,29 @@ class CalendarHorizontalController: HorizontalSnappingController, UICollectionVi
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: dayCellId, for: indexPath) as! CalendarDayCell
         
+        if indexPath == selectedIndexPath {
+            cell.configureForSelectedState()
+        } else {
+            cell.configureForDeselectedState()
+        }
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return 12
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if let previousIndexPath = selectedIndexPath,
+           let previousCell = collectionView.cellForItem(at: previousIndexPath) as? CalendarDayCell {
+            previousCell.configureForDeselectedState()
+        }
+        
+        if let selectedCell = collectionView.cellForItem(at: indexPath) as? CalendarDayCell {
+            selectedCell.configureForSelectedState()
+        }
+        
+        selectedIndexPath = indexPath
     }
     
 }
--- TaskManager_iOS/Views/Calendar/CalendarDayCell.swift
+++ TaskManager_iOS/Views/Calendar/CalendarDayCell.swift
@@ -6,17 +6,88 @@
 //
 
 import UIKit
+import SnapKit
 
 class CalendarDayCell: UICollectionViewCell {
+    
+    let monthLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Feb"
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 14, weight: .bold)
+        
+        return label
+    }()
+    
+    let dayLabel: UILabel = {
+        let label = UILabel()
+        label.text = "20"
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 20)
+        
+        return label
+    }()
+    
+    let weekLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Tue"
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 14, weight: .bold)
+        
+        return label
+    }()
+    
+    let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        stackView.distribution = .fillEqually
+        
+        return stackView
+    }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        backgroundColor = .blue
+        backgroundColor = .white
+        setupUI()
+        setupConstraints()
+    }
+    
+    func setupUI() {
+        layer.borderWidth = 1.5
+        layer.borderColor = UIColor(red: 16, green: 94, blue: 245).cgColor
+        layer.cornerRadius = 14
+        clipsToBounds = true
+        
+        stackView.addArrangedSubview(monthLabel)
+        stackView.addArrangedSubview(dayLabel)
+        stackView.addArrangedSubview(weekLabel)
+        contentView.addSubview(stackView)
+    }
+    
+    func setupConstraints() {
+        stackView.snp.makeConstraints { make in
+            make.edges.equalToSuperview().inset(5)
+        }
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureForSelectedState() {
+        backgroundColor = UIColor(red: 16, green: 94, blue: 245)
+        monthLabel.textColor = .white
+        dayLabel.textColor = .white
+        weekLabel.textColor = .white
+    }
+    
+    func configureForDeselectedState() {
+        backgroundColor = .white
+        monthLabel.textColor = .black
+        dayLabel.textColor = .black
+        weekLabel.textColor = .black
     }
     
 }
