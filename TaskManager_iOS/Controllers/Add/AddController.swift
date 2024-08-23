f0e4f2efb995a3f9b838764f0eeb4d8dc85ab05a
F4rab1 <issafarabi2022@gmail.com>
2024-08-23 23:13:43 +0500

    Add Task started

--- TaskManager_iOS/Controllers/Add/AddController.swift
+++ TaskManager_iOS/Controllers/Add/AddController.swift
@@ -6,13 +6,102 @@
 //
 
 import UIKit
+import SnapKit
 
 class AddController: UIViewController {
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 24)
+        label.text = "Add Task"
+        return label
+    }()
+    
+    let startDateLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Start Date:"
+        label.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        label.textColor = UIColor(red: 23, green: 162, blue: 184)
+        return label
+    }()
 
+    let startDateButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Feb-22-2024", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 14)
+        button.setTitleColor(.darkGray, for: .normal)
+        button.backgroundColor = UIColor(red: 234, green: 255, blue: 255)
+        button.layer.borderColor = UIColor(red: 133, green: 246, blue: 255).cgColor
+        button.layer.borderWidth = 0.5
+        button.layer.cornerRadius = 10
+        return button
+    }()
+
+    let endDateLabel: UILabel = {
+        let label = UILabel()
+        label.text = "End Date:"
+        label.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        label.textColor = UIColor(red: 23, green: 162, blue: 184)
+        return label
+    }()
+
+    let endDateButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Feb-22-2024", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 14)
+        button.setTitleColor(.darkGray, for: .normal)
+        button.layer.borderColor = UIColor(red: 133, green: 246, blue: 255).cgColor
+        button.layer.borderWidth = 0.5
+        button.layer.cornerRadius = 10
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = UIColor(red: 249, green: 252, blue: 254)
+        
+        setupUI()
+        setupConstraints()
     }
-
+    
+    func setupUI() {
+        view.backgroundColor = .white
+        
+        view.addSubview(titleLabel)
+        view.addSubview(startDateLabel)
+        view.addSubview(endDateLabel)
+        view.addSubview(startDateButton)
+        view.addSubview(endDateButton)
+    }
+    
+    func setupConstraints() {
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(20)
+            make.centerX.equalTo(view.snp.centerX)
+        }
+        
+        startDateLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(20)
+            make.leading.equalToSuperview().inset(20)
+        }
+        
+        endDateLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(20)
+            make.leading.equalTo(view.snp.centerX).offset(10)
+        }
+        
+        startDateButton.snp.makeConstraints { make in
+            make.top.equalTo(startDateLabel.snp.bottom).offset(7)
+            make.leading.equalToSuperview().inset(20)
+            make.trailing.equalTo(view.snp.centerX).offset(-10)
+            make.height.equalTo(50)
+        }
+        
+        endDateButton.snp.makeConstraints { make in
+            make.top.equalTo(endDateLabel.snp.bottom).offset(7)
+            make.leading.equalTo(view.snp.centerX).offset(10)
+            make.trailing.equalToSuperview().inset(20)
+            make.height.equalTo(50)
+        }
+    }
+    
 }
