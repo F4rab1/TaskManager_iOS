8566e5517dcfe80e58d09145e6d471c56d37e236
F4rab1 <issafarabi2022@gmail.com>
2024-08-30 23:56:13 +0500

    Token Model

---
+++ TaskManager_iOS/Models/Token.swift
@@ -0,0 +1,13 @@
+//
+//  Token.swift
+//  TaskManager_iOS
+//
+//  Created by Фараби Иса on 30.08.2024.
+//
+
+import Foundation
+
+struct TokenResponse: Codable {
+    let access: String
+    let refresh: String
+}
