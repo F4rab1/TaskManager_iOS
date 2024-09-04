4432b1e7fd7f7808e83f0a3b3bd832c913a990a5
F4rab1 <issafarabi2022@gmail.com>
2024-09-04 22:25:51 +0500

    Get request

--- TaskManager_iOS/Service/APIService.swift
+++ TaskManager_iOS/Service/APIService.swift
@@ -15,6 +15,44 @@ class APIService {
     
     var accessToken: String? = AuthManager.shared.accessToken
     
+    func get(endpoint: String, completion: @escaping (Result<Data, Error>) -> Void) {
+        guard let url = URL(string: baseURL + endpoint) else {
+            completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"])))
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse else {
+                let error = NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Invalid response"])
+                completion(.failure(error))
+                return
+            }
+            
+            let statusCode = httpResponse.statusCode
+            if 200...299 ~= statusCode {
+                if let data = data {
+                    completion(.success(data))
+                } else {
+                    let error = NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Empty response data"])
+                    completion(.failure(error))
+                }
+            } else {
+                let error = NSError(domain: "", code: statusCode, userInfo: [NSLocalizedDescriptionKey: "Request failed with status code: \(statusCode)"])
+                completion(.failure(error))
+            }
+        }
+        task.resume()
+    }
+    
     func post(endpoint: String, parameters: [String: Any], completion: @escaping (Result<Data, Error>) -> Void) {
         guard let url = URL(string: baseURL + endpoint) else { return }
         var request = URLRequest(url: url)
