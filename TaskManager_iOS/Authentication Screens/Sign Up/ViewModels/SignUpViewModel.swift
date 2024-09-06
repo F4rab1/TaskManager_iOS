0ce312f48b84d7aa3f850dc0f240d2d53da401ed
F4rab1 <issafarabi2022@gmail.com>
2024-09-06 22:53:16 +0500

    SignUp ViewModel

--- TaskManager_iOS/Authentication Screens/Sign Up/ViewModels/SignUpViewModel.swift
+++ TaskManager_iOS/Authentication Screens/Sign Up/ViewModels/SignUpViewModel.swift
@@ -14,3 +14,20 @@ protocol SignUpProtocol {
     
     func register(username: String, password: String, password2: String)
 }
+
+class SignUpViewModel: SignUpProtocol {
+    
+    var isRegistered: Bool = false
+    
+    var registerResult: ((Result<Data, Error>) -> Void)?
+    
+    func register(username: String, password: String, password2: String) {
+        
+        let parameters: [String: Any] = ["username": username, "password": password, "password2": password2]
+        
+        APIService.shared.post(endpoint: "customers", parameters: parameters) { [weak self] result in
+//
+            
+        }
+    }
+}
