7ee1414139d3b2a175edc0053fa2383987e7e366
F4rab1 <issafarabi2022@gmail.com>
2024-12-03 23:53:58 +0100

    All tasks page implemented

--- TaskManager_iOS/Controllers/Home/HomeController.swift
+++ TaskManager_iOS/Controllers/Home/HomeController.swift
@@ -11,13 +11,47 @@ class HomeController: UIViewController {
     
     private var tasks: [Task]?
     
+    let allTaskButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("All Tasks", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.backgroundColor = UIColor(red: 249, green: 252, blue: 254)
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor(red: 23, green: 162, blue: 184).cgColor
+        button.layer.cornerRadius = 10
+        button.layer.masksToBounds = true
+        
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupUI()
+        setupConstraints()
         
         APIService.shared.fetchTasks { res, err in
             self.tasks = res
         }
         
+    }
+    
+    func setupUI() {
+        view.addSubview(allTaskButton)
+        
+        allTaskButton.addTarget(self, action: #selector(allTaskButtonhandle), for: .touchUpInside)
+    }
+    
+    @objc func allTaskButtonhandle() {
+        let tasksController = TasksListController()
+        tasksController.tasks = self.tasks
+        navigationController?.pushViewController(tasksController, animated: true)
+    }
+    
+    func setupConstraints() {
+        allTaskButton.snp.makeConstraints { make in
+            make.top.leading.equalTo(view.safeAreaLayoutGuide).offset(20)
+        }
     }
     
 }
---
+++ TaskManager_iOS/Controllers/Home/TasksListController.swift
@@ -0,0 +1,121 @@
+//
+//  TasksListController.swift
+//  TaskManager_iOS
+//
+//  Created by Фараби Иса on 03.12.2024.
+//
+
+import UIKit
+
+class TasksListController: UIViewController {
+    
+    var tasks: [Task]?
+    
+    let tableView: UITableView = {
+        let tv = UITableView()
+        tv.separatorStyle = .none
+        tv.backgroundColor = UIColor(red: 249, green: 252, blue: 254)
+        
+        return tv
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Home Page"
+        view.backgroundColor = .white
+        
+        setupUI()
+        setupConstraints()
+        setupTableView()
+        
+    }
+    
+    func setupUI() {
+        view.addSubview(tableView)
+    }
+    
+    func setupConstraints() {
+        tableView.snp.makeConstraints { make in
+            make.top.leading.trailing.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+    
+    func setupTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(TaskTableViewCell.self, forCellReuseIdentifier: "TaskCell")
+    }
+    
+}
+
+extension TasksListController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskCell", for: indexPath) as! TaskTableViewCell
+        
+        let task = tasks?[indexPath.row]
+        cell.categoryLabel.text = task?.category
+        cell.titleLabel.text = task?.title
+        if task?.stage == "completed" {
+            cell.isCompleted = true
+        } else {
+            cell.isCompleted = false
+        }
+        switch task?.priority {
+        case 1:
+            cell.priorityLabel.text = "LOW"
+        case 2:
+            cell.priorityLabel.text = "MEDIUM"
+        case 3:
+            cell.priorityLabel.text = "HIGH"
+        default:
+            cell.priorityLabel.text = "NULL"
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let taskDetailController = TaskDetailController()
+        taskDetailController.task = tasks?[indexPath.row]
+        navigationController?.pushViewController(taskDetailController, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 130
+    }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let deleteAction = UIContextualAction(style: .destructive, title: nil) { [weak self] (action, view, completionHandler) in
+            self?.deleteTask(at: indexPath)
+            completionHandler(true)
+        }
+        deleteAction.image = createCustomDeleteImage()
+        deleteAction.backgroundColor = UIColor(red: 249, green: 252, blue: 254)
+        
+        let configuration = UISwipeActionsConfiguration(actions: [deleteAction])
+        return configuration
+    }
+    
+    private func deleteTask(at indexPath: IndexPath) {
+        guard let task = tasks?[indexPath.row] else { return }
+        
+        tasks?.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .automatic)
+        
+        APIService.shared.deleteTask(taskId: task.id) { error in
+            if let error = error {
+                print("Failed to delete task:", error)
+            } else {
+                print("Task successfully deleted from backend")
+            }
+        }
+    }
+    
+}
+
